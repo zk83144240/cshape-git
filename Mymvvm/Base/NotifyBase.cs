a973ee67cafb0b171c5e41c86d46a17d8bdf858e
zk83144240 <83144240@qq.com>
2024-11-02 02:18:14 +0800

    mymvvm

---
+++ Mymvvm/Base/NotifyBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mymvvm.Base
+{
+   public class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (!Equals(field, value)|| field == null)
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Mymvvm/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Mymvvm.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mymvvm.ViewModels
+{
+    public class MainViewModel : NotifyBase
+    {
+        private int _value;
+
+
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                SetProperty<int>(ref _value, value);
+
+
+            }
+
+        }
+
+
+
+        public MainViewModel()
+        {
+            Value = 123;
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(1000);
+                    Value++;
+                }
+            });
+
+        }
+    }
+}
