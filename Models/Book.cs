6fac70a0a133d800978615b5f23df724211429b1
zk83144240 <83144240@qq.com>
2025-02-23 19:24:18 +0800

    Reapply "Mvvm+toolkit+sqlite+依赖注入+数据上下文=图书管理系统，实现增删改查"
    This reverts commit 801559cdded4d2a06cb4c681ef82a68fbcf38f21.

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MyBooksManager.Models;
+
+
+public partial class Book:ObservableObject
+{
+    
+    [ObservableProperty] private int _id;
+    
+    [ObservableProperty] private string _title;
+   
+    [ObservableProperty] private string _author;
+
+}  
